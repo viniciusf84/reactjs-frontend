dc2c4da6d0c81c7c86103eacec503745836acde0
inicius <viniciusf84@hotmail.com>
2020-08-19 00:11:18 -0300

    Add and delete functions

--- src/App.js
+++ src/App.js
@@ -1,29 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [newRepositoryTitle, setNewRepositoryTitle] = useState('');
+  const [newRepositoryUrl, setNewRepositoryUrl] = useState('');
+
+  async function listRepositories() {
+    try {
+      const response = await api.get('repositories');
+
+      setRepositories(response.data);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const repositoryToSubmit = {	
+      "title": newRepositoryTitle,
+      "url": newRepositoryUrl,
+      "techs": []
+    };
+
+    try {
+      const response = await api.post('repositories', repositoryToSubmit);
+
+      const repoToAdd = response.data;
+
+      setRepositories([...repositories, repoToAdd]);
+      setNewRepositoryTitle('');
+      setNewRepositoryUrl('');
+      
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`).then(response => {
+      if(response) {
+        setRepositories(repositories.filter(repo => repo.id !== id));
+      }
+    });    
   }
+
+  useEffect(() => {
+    listRepositories();
+  }, []);
 
   return (
     <div>
+      
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
+      
+      <section id="add-new-repository">      
+        <h2>Adicionar novo repositório</h2>
+        <fieldset>
+          <label htmlFor="title">Título do respoitório</label>
+          <input 
+            id="title" 
+            type="text" 
+            name="title"
+            value={newRepositoryTitle} 
+            onChange={e => setNewRepositoryTitle(e.target.value)} 
+          />          
+        </fieldset>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+        <fieldset>
+          <label htmlFor="url">Url do respoitório</label>
+          <input 
+            id="url" 
+            type="text" 
+            name="url"
+            value={newRepositoryUrl} 
+            onChange={e => setNewRepositoryUrl(e.target.value)} 
+          />          
+        </fieldset>
+
+        <button onClick={handleAddRepository}>Adicionar</button>          
+      </section>
+
     </div>
   );
 }
